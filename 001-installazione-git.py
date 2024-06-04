31b441a37a476bfc230ad287db89ac1345f4674d
Damiano <pozzatodamiano@gmail.com>
2024-06-05 00:04:07 +0200

    first commit

---
+++ 001-installazione-git.py
@@ -0,0 +1,8 @@
+### installazione di GIT
+
+# creiamo un profilo github
+# installiamo github così: 
+# aprire microsoft powershell e eseguire il comando:
+# winget install --id Git.Git -e --source winget
+
+# configurazione della ssh key
